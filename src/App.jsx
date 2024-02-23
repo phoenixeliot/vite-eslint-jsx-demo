79889ce9d9fd63befa8f14945cffe2958cc7748d
Phoenix Eliot <phoenixjeliot@gmail.com>
2024-02-22 20:38:47 -0500

    Add example ESLint error-causing code

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+for (var i = 0; i < 10; i--) { }
+
 function App() {
   const [count, setCount] = useState(0)
 
